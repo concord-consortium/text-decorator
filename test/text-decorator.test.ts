7d0b1789a492ed1f7f098b278925150877b19929
Kirk Swenson <kswenson@concord.org>
2018-06-19 10:48:12 -0700

    Require semicolons; add `lint:fix` script

--- test/text-decorator.test.ts
+++ test/text-decorator.test.ts
@@ -1,14 +1,14 @@
-import TextDecorator from "../src/text-decorator"
+import TextDecorator from "../src/text-decorator";
 
 /**
  * Dummy test
  */
 describe("Dummy test", () => {
   it("works if true is truthy", () => {
-    expect(true).toBeTruthy()
-  })
+    expect(true).toBeTruthy();
+  });
 
   it("TextDecorator is instantiable", () => {
-    expect(new TextDecorator()).toBeInstanceOf(TextDecorator)
-  })
-})
+    expect(new TextDecorator()).toBeInstanceOf(TextDecorator);
+  });
+});
