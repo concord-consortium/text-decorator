f2ff593c5efd2758d8fe342fa6b554f9b8380d7b
pjanik <janikpiotrek@gmail.com>
2018-10-05 16:03:09 +0200

    Updates

--- test/test-utils.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Utility functions for use in tests.
- */
-export function dispatchSimulatedEvent(elt: HTMLElement, type: string) {
-  // cf. https://stackoverflow.com/a/27557936
-  const event = document.createEvent("HTMLEvents");
-  event.initEvent(type, false, true);
-  elt.dispatchEvent(event);
-}
