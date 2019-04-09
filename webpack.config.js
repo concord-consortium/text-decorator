4567e3260b98ad9e839795d7cac6647a568d55dc
pjanik <janikpiotrek@gmail.com>
2019-04-09 15:27:37 +0200

    Fix externals config and typings - make them available in NPM module
    [#165062314]

--- webpack.config.js
+++ webpack.config.js
@@ -55,8 +55,18 @@ module.exports = (env, argv) => {
       warningsFilter: /export .* was not found in/
     },
     externals: {
-      'react': 'React',
-      'react-dom': 'ReactDOM'
+      'react': {
+        commonjs: 'react',
+        commonjs2: 'react',
+        amd: 'react',
+        root: 'React'
+      },
+      'react-dom': {
+        commonjs: 'react-dom',
+        commonjs2: 'react-dom',
+        amd: 'react-dom',
+        root: 'ReactDOM'
+      }
     },
     plugins: [
       new ForkTsCheckerWebpackPlugin()
